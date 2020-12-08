578d3a7a787bc4e25552952eb71218a0d2965634
Ivan <43240272+pinkyhi@users.noreply.github.com>
2020-12-08 18:24:16 +0200

    Glossary part

---
+++ Authentification/API/Routes/DefaultRoutes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentification.API.Routes
+{
+    public static class DefaultRoutes
+    {
+        public const string Root = "api";
+
+        public const string Version = "v0";
+
+        public const string Base = Root + "/" + Version;
+
+        public static class Glossary
+        {
+            public const string Main = Base + "/glossary";
+        }
+    }
+}
---
+++ Authentification/Controllers/GlossaryController.cs
@@ -0,0 +1,38 @@
+﻿using Authentification.API.Routes;
+using Authentification.API.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authentification.Controllers
+{
+    [ApiController]
+    public class GlossaryController : ControllerBase
+    {
+        private static List<GlossaryItem> Glossary = new List<GlossaryItem> {
+            new GlossaryItem
+            {
+                Term= "Access Token",
+                Definition = "A credential that can be used by an application to access an API. It informs the API that the bearer of the token has been authorized to access the API and perform specific actions specified by the scope that has been granted."
+            },
+            new GlossaryItem
+            {
+                Term= "JWT",
+                Definition = "An open, industry standard RFC 7519 method for representing claims securely between two parties. "
+            },
+            new GlossaryItem
+            {
+                Term= "OpenID",
+                Definition = "An open standard for authentication that allows applications to verify users are who they say they are without needing to collect, store, and therefore become liable for a user’s login information."
+            }
+        };
+
+        [HttpGet(DefaultRoutes.Glossary.Main)]
+        public ActionResult<List<GlossaryItem>> Get()
+        {
+            return Ok(Glossary);
+        }
+    }
+}
